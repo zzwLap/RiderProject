6ca9857923de440ecb531e27b7de13763d9dad91
zzwLap <806804985@qq.com>
2023-07-05 10:49:50 +0800

    Changed Message

--- VCSBasics/Program.cs
+++ VCSBasics/Program.cs
@@ -6,7 +6,8 @@ namespace VCSBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Git!");
+            //dsafdsaf
         }
     }
 }
